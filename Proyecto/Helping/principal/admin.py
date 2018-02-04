762138f1998f7107869c154caac8ea4bac1ceb8b
Carlos Alejandro Hernández Mejía <32623757+JandroMejia97@users.noreply.github.com>
2018-02-04 17:55:58 -0600

    Modificando modelos
    Agregué los modelos a la vista del admin y modifiqué la visibilidad de los campos

--- Proyecto/Helping/principal/admin.py
+++ Proyecto/Helping/principal/admin.py
@@ -2,11 +2,26 @@ from django.contrib import admin
 from . import models
 
 
+class FacultadInline(admin.StackedInline):
+    model = models.Facultad
+    extra = 1
+
+
 @admin.register(models.Universidad)
 class UniversidadAdmin(admin.ModelAdmin):
     list_display = ['nombre_universidad', 'acronimo_universidad', 'contacto', 'pagina_web']
     fieldsets = [
         ('Detalles', {'fields': ['nombre_universidad', 'acronimo_universidad']}),
+        ('Información de contacto', {'fields': ['contacto', 'pagina_web']}),
+    ]
+    inlines = [FacultadInline]
+
+
+@admin.register(models.Facultad)
+class FacultadAdmin(admin.ModelAdmin):
+    list_display = ['nombre_facultad', 'universidad', 'pagina_web']
+    fieldsets = [
+        ('Detalles', {'fields': ['nombre_facultad', 'universidad']}),
         ('Información de contacto', {'fields': ['contacto', 'pagina_web']}),
     ]
 
--- Proyecto/Helping/principal/models.py
+++ Proyecto/Helping/principal/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+
 class Universidad(models.Model):
     nombre_universidad = models.CharField(max_length=80, blank=False)
     acronimo_universidad = models.CharField(max_length=10, blank=False, unique=True)
@@ -13,4 +14,19 @@ class Universidad(models.Model):
     class Meta:
         verbose_name = 'Universidad'
         verbose_name_plural = 'Universidades'
+        ordering = ['id']
+
+
+class Facultad(models.Model):
+    nombre_facultad = models.CharField(max_length=80, blank=False)
+    universidad = models.ForeignKey('Universidad', on_delete=models.CASCADE,)
+    pagina_web = models.URLField(verbose_name='Página Web', unique=True)
+    contacto = models.PositiveIntegerField(verbose_name='Contacto', unique=True)
+
+    def __str__(self):
+        return self.nombre_facultad
+
+    class Meta:
+        verbose_name = 'Facultad'
+        verbose_name_plural = 'Facultades'
         ordering = ['id']
